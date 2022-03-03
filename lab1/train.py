8ba9cd9d4508dbc8c476566a22a834ac5e6eaf0f
bhbbbbb <smark9005310@gmail.com>
2022-03-04 03:17:50 +0800

    refine train tool

--- lab1/train.py
+++ lab1/train.py
@@ -8,13 +8,30 @@ layers_list = [1, 2, 4, 8, 16]
 def train_all():
     for layers in layers_list:
         for neurons in neurons_list:
-            model = MLP(neurons=neurons, layers=layers, epochs=16)
-            model.start_train(trainloader)
+            model = MLP(neurons=neurons, layers=layers, epochs=16, learning_rate=1)
+            model.start_train(trainloader, checkpoints=epochs_list)
             model.start_eval(testloader)
     return
+
+def train_from_checkpoint(log_path: str, epochs: int, checkpoints: list):
+    import torch
+    log = torch.load(log_path)
+    log["epochs"] = epochs
+
+    print(f"load weigths from: {log_path}")
+    model = MLP(**log)
+    model.start_train(trainloader, checkpoints=checkpoints)
+    model.start_eval(testloader)
+    return
+
 
 if __name__ == "__main__":
     
     trainloader, testloader = Dataset().load()
-    train_all()
+    model = MLP(neurons=16, layers=16, epochs=1024, learning_rate=0.0001)
+    model.start_train(trainloader, checkpoints=[(16*i) for i in range(1, (1024//16)+1)])
+    model.start_eval(testloader)
+    # train_from_checkpoint(log_path="D:\Documents\\1102\AI\ml-practices\lab1\model\log\\relu\l16_n16\l16_n16_e16_of_16",
+    #     epochs=128, checkpoints=[(16*i) for i in range(1, (1024 // 16) + 1)])
+    # train_all()
 
