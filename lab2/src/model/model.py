fa2827b3a81d6634ede078e71e05a8e8a33e39d5
bhbbbbb <smark9005310@gmail.com>
2022-04-01 15:26:05 +0800

    add dropout layers

--- lab2/src/model/model.py
+++ lab2/src/model/model.py
@@ -43,60 +43,69 @@ class _BaseNN(nn.Module):
         
 
 class FatLeNet5(_BaseNN):
-    def __init__(self, config: Config, batch_norm: bool = True):
+    def __init__(self, config: Config, batch_norm: bool = True, dropout: bool = True):
         super().__init__()
         self.seq = nn.Sequential()
 
         self.append(nn.Conv2d(3, 6, kernel_size=5, stride=1))
-        self.append(nn.ReLU())
+        if dropout: self.append(nn.Dropout2d(inplace=True))
+        self.append(nn.ReLU(inplace=True))
         if batch_norm: self.append(nn.BatchNorm2d(6))
         self.append(nn.AvgPool2d(kernel_size=2, stride=2))
 
         self.append(nn.Conv2d(6, 16, kernel_size=5, stride=1))
-        self.append(nn.ReLU())
+        if dropout: self.append(nn.Dropout2d(inplace=True))
+        self.append(nn.ReLU(inplace=True))
         if batch_norm: self.append(nn.BatchNorm2d(16))
         self.append(nn.AvgPool2d(kernel_size=2, stride=2))
 
         self.append(nn.Conv2d(16, 120, kernel_size=53))
-        self.append(nn.ReLU())
+        if dropout: self.append(nn.Dropout2d(inplace=True))
+        self.append(nn.ReLU(inplace=True))
         if batch_norm: self.append(nn.BatchNorm2d(120))
 
         self.append(nn.Flatten())
 
         self.append(nn.Linear(120, 84))
-        self.append(nn.ReLU())
+        if dropout: self.append(nn.Dropout(inplace=True))
+        self.append(nn.ReLU(inplace=True))
 
         self.append(nn.Linear(84, config.NUM_CLASS))
         self.append(nn.Softmax(dim=1))
 
 
 class FakeVGG16(_BaseNN):
-    def __init__(self, config: Config, batch_norm: bool = True):
+    def __init__(self, config: Config, batch_norm: bool = True, dropout: bool = True):
         super().__init__()
         
         self.seq = nn.Sequential()
 
         self.append(nn.Conv2d(3, 64, kernel_size=3, stride=1, padding="same"))
+        if dropout: self.append(nn.Dropout2d(inplace=True))
         self.append(nn.ReLU())
         if batch_norm: self.append(nn.BatchNorm2d(64))
         self.append(nn.MaxPool2d(kernel_size=2, stride=2)) # 112 x 112
 
         self.append(nn.Conv2d(64, 128, kernel_size=3, stride=1, padding="same"))
+        if dropout: self.append(nn.Dropout2d(inplace=True))
         self.append(nn.ReLU())
         if batch_norm: self.append(nn.BatchNorm2d(128))
         self.append(nn.MaxPool2d(kernel_size=2, stride=2)) # 56 x 56
 
         self.append(nn.Conv2d(128, 256, kernel_size=3, stride=1, padding="same"))
+        if dropout: self.append(nn.Dropout2d(inplace=True))
         self.append(nn.ReLU())
         if batch_norm: self.append(nn.BatchNorm2d(256))
         self.append(nn.MaxPool2d(kernel_size=2, stride=2)) # 28 x 28
 
         self.append(nn.Conv2d(256, 512, kernel_size=3, stride=1, padding="same"))
+        if dropout: self.append(nn.Dropout2d(inplace=True))
         self.append(nn.ReLU())
         if batch_norm: self.append(nn.BatchNorm2d(512))
         self.append(nn.MaxPool2d(kernel_size=2, stride=2)) # 14 x 14
 
         self.append(nn.Conv2d(512, 512, kernel_size=3, stride=1, padding="same"))
+        if dropout: self.append(nn.Dropout2d(inplace=True))
         self.append(nn.ReLU())
         if batch_norm: self.append(nn.BatchNorm2d(512))
         self.append(nn.MaxPool2d(kernel_size=14, stride=14))
@@ -104,9 +113,11 @@ class FakeVGG16(_BaseNN):
         self.append(nn.Flatten())
 
         self.append(nn.Linear(512, 512))
+        if dropout: self.append(nn.Dropout(inplace=True))
         self.append(nn.ReLU())
 
         self.append(nn.Linear(512, 128))
+        if dropout: self.append(nn.Dropout(inplace=True))
         self.append(nn.ReLU())
 
         self.append(nn.Linear(128, config.NUM_CLASS))
