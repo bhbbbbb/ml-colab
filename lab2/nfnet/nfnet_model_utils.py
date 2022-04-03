9feef08a83d1398de2cfd10f8ca15d1c89c7ae43
bhbbbbb <smark9005310@gmail.com>
2022-04-04 01:57:44 +0800

    refine nfnet utils

--- lab2/nfnet/nfnet_model_utils.py
+++ lab2/nfnet/nfnet_model_utils.py
@@ -1,19 +1,44 @@
+import os
 from typing import Tuple
 import torch
 from torch import Tensor
 import torch.cuda.amp as amp
 from tqdm import tqdm
 from imgclf.base.model_utils import BaseModelUtils
-# from imgclf.nfnets import SGD_AGC, pretrained_nfnet # pylint: disable=no-name-in-module
-from nfnets import SGD_AGC, pretrained_nfnet # pylint: disable=no-name-in-module
+from imgclf.base.model_utils.model_utils import ModelStates, HistoryUtils
+from imgclf.base.logger import Logger
+from imgclf.nfnets import SGD_AGC, pretrained_nfnet # pylint: disable=no-name-in-module
+# from nfnets import SGD_AGC, pretrained_nfnet # pylint: disable=no-name-in-module
 from imgclf.dataset import Dataset
+from imgclf.nfnets.nfnets.model import NFNet
 from .config import NfnetConfig
 
 class NfnetModelUtils(BaseModelUtils):
 
-    def __init__(self, pretrained_path: str, config: NfnetConfig):
-
-        model = pretrained_nfnet(pretrained_path)
+    def __init__(
+        self,
+        model: torch.nn.modules,
+        config: NfnetConfig,
+        optimizer: torch.optim.Optimizer,
+        start_epoch: int,
+        root: str,
+        history_utils,
+        logger,
+    ):
+        self.scaler = amp.GradScaler()
+        super().__init__(
+            model = model,
+            config = config,
+            optimizer = optimizer,
+            start_epoch = start_epoch,
+            root = root,
+            history_utils = history_utils,
+            logger = logger,
+        )
+        return
+    
+    @staticmethod
+    def _inti_model_optimizer(model: NFNet, config: NfnetConfig):
 
         optimizer = SGD_AGC(
             # The optimizer needs all parameter names 
@@ -28,17 +53,62 @@ class NfnetModelUtils(BaseModelUtils):
         # Find desired parameters and exclude them 
         # from weight decay and clipping
         for group in optimizer.param_groups:
-            name = group['name'] 
+            name = group["name"] 
             
             if model.exclude_from_weight_decay(name):
-                group['weight_decay'] = 0
+                group["weight_decay"] = 0
 
             if model.exclude_from_clipping(name):
-                group['clipping'] = None
+                group["clipping"] = None
+        return model, optimizer
+    
+    @staticmethod
+    def init_model(config: NfnetConfig):
+        model = NFNet(
+            num_classes = config.num_class,
+            variant = config["variant"], 
+            stochdepth_rate=config["stochdepth_rate"], 
+            alpha=config["alpha"],
+            se_ratio=config["se_ratio"],
+            activation=config["activation"]
+        )
+        return model
+    
+    @classmethod
+    def start_new_training_from_pretrained(cls, pretrained_path: str, config: NfnetConfig):
 
-        # criterion = nn.CrossEntropyLoss()
-        self.scaler = amp.GradScaler()
-        return
+        pretrained_model = pretrained_nfnet(pretrained_path)
+        model = cls.init_model(config)
+        model.load_state_dict(pretrained_model.state_dict())
+        model, optimizer = cls._inti_model_optimizer(model, config)
+
+        return super().start_new_training(model, config, optimizer)
+    
+    @classmethod
+    def load_checkpoint(cls, model: NFNet, checkpoint_path: str,
+                        config: NfnetConfig, optimizer = None):
+        assert os.path.isfile(checkpoint_path)
+
+        tem = torch.load(checkpoint_path)
+        checkpoint = ModelStates(**tem)
+
+        model, optimizer = cls._inti_model_optimizer(model, config)
+        optimizer.load_state_dict(checkpoint.optimizer_state_dict)
+        
+        root = os.path.dirname(checkpoint_path)
+        logger = Logger(root)
+        start_epoch = checkpoint.start_epoch
+        history_utils = HistoryUtils.load_history(root, start_epoch, logger)
+        logger.log(f"Checkpoint {os.path.basename(checkpoint_path)} is loaded.")
+        return cls(
+            model = model,
+            config = config,
+            optimizer = optimizer,
+            start_epoch = start_epoch,
+            root = root,
+            history_utils = history_utils,
+            logger = logger,
+        )
 
     def _train_epoch(self, train_dataset: Dataset) -> Tuple[float, float]:
         self.model.train()
@@ -48,13 +118,13 @@ class NfnetModelUtils(BaseModelUtils):
         for inputs, targets in tqdm(dataloader):
 
             inputs: Tensor = inputs.half().to(self.config.device)\
-                if self.config['use_fp16'] else inputs.to(self.config.device)
+                if self.config["use_fp16"] else inputs.to(self.config.device)
             
             targets: Tensor = targets.to(self.config.device)
 
             self.optimizer.zero_grad()
 
-            with amp.autocast(enabled=self.config['amp']):
+            with amp.autocast(enabled=self.config["amp"]):
                 output = self.model(inputs)
             loss: Tensor = self.criterion(output, targets)
             
@@ -70,7 +140,7 @@ class NfnetModelUtils(BaseModelUtils):
 
         # epoch_padding = int(math.log10(epochs) + 1)
         # batch_padding = int(math.log10(len(dataloader.dataset)) + 1)
-        # print(f"\rEpoch {epoch+1:0{epoch_padding}d}/{config['epochs']}"
+        # print(f"\rEpoch {epoch+1:0{epoch_padding}d}/{config["epochs"]}"
         #     f"\tImg {processed_imgs:{batch_padding}d}/{len(dataloader.dataset)}"
         #     f"\tLoss {running_loss / (step+1):6.4f}"
         #     f"\tAcc {100.0*correct_labels/processed_imgs:5.3f}%\t",
